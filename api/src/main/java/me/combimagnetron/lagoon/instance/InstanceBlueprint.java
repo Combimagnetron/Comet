8b1ac7e67f74ecf83f4db1ff13a359cae583cd1b
Combimagnetron <56763839+Combimagnetron@users.noreply.github.com>
2023-07-03 20:55:26 +0200

    Add new messages and listener
    Adds two new message types (ServiceStatusChangeRequest & RequestInstanceBlueprints), used to communicate with the Pilot service.
    Adds two new listener classes that listen to the newly added messages types on the Pilot service side.

--- api/src/main/java/me/combimagnetron/lagoon/instance/InstanceBlueprint.java
+++ api/src/main/java/me/combimagnetron/lagoon/instance/InstanceBlueprint.java
@@ -1,14 +1,23 @@
 package me.combimagnetron.lagoon.instance;
 
+import me.combimagnetron.lagoon.Lagoon;
 import me.combimagnetron.lagoon.communication.MessageListener;
+import me.combimagnetron.lagoon.communication.message.MessageChannel;
+import me.combimagnetron.lagoon.communication.message.impl.servicebound.ServiceBoundRequestInstanceBlueprintsMessage;
+import me.combimagnetron.lagoon.data.Identifier;
 import me.combimagnetron.lagoon.util.VersionCollection;
 
 import java.io.IOException;
 import java.io.Serializable;
 import java.util.Date;
+import java.util.Optional;
+import java.util.concurrent.atomic.AtomicReference;
+
+import static me.combimagnetron.lagoon.operation.Operations.async;
 
 public class InstanceBlueprint implements Serializable {
     private final Info info;
+    private VersionCollection<InstanceBlueprint> versionCollection;
 
     public InstanceBlueprint(Info info) {
         this.info = info;
@@ -16,14 +25,20 @@ public class InstanceBlueprint implements Serializable {
 
 
 
-    public record Info(String version, Date created, String author) implements Serializable {
-
+    public record Info(Identifier identifier, String version, Date created, String author) implements Serializable {
+        public static Info info(Identifier identifier, String version, Date created, String author) {
+            return new Info(identifier, version, created, author);
+        }
     }
 
-    public static VersionCollection<InstanceBlueprint> request() {
-
+    public static VersionCollection<InstanceBlueprint> request(Identifier identifier) {
+        final MessageChannel channel = async(Lagoon.messageClient().channel(Identifier.of("service", "pilot")));
+        async(channel.send(new ServiceBoundRequestInstanceBlueprintsMessage(identifier)));
+        AtomicReference<VersionCollection<InstanceBlueprint>> atomicReference = new AtomicReference<>();
+        channel.awaitMessage(ServiceBoundRequestInstanceBlueprintsMessage.Response.class, (message) -> {
+            atomicReference.set(((ServiceBoundRequestInstanceBlueprintsMessage.Response) message).versionCollection());
+        });
+        return atomicReference.get();
     }
-
-    class ResponseMessageListener implements MessageListener<>
 
 }
