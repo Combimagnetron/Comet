6d1d6bedc4c4aa15133df68cee62d05b81f9d0e1
Combimagnetron <56763839+Combimagnetron@users.noreply.github.com>
2023-07-26 19:38:18 +0200

    feat(n/a): add target type to command annotation

--- api/src/main/java/me/combimagnetron/lagoon/command/annotations/Command.java
+++ api/src/main/java/me/combimagnetron/lagoon/command/annotations/Command.java
@@ -3,10 +3,13 @@ package me.combimagnetron.lagoon.command.annotations;
 import me.combimagnetron.lagoon.command.argument.Argument;
 import me.combimagnetron.lagoon.user.User;
 
+import java.lang.annotation.ElementType;
 import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
 
 @Retention(RetentionPolicy.RUNTIME)
+@Target({ElementType.TYPE})
 public @interface Command {
     String command();
 
