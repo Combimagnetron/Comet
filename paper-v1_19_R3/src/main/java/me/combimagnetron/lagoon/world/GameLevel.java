a3216c967c5ef3ede8f597d6c78b61e1f5e41dfe
Combimagnetron <alec@tikvout.nl>
2023-08-04 23:14:22 +0200

    feat(n/a): implement GameLevel world to load meridian worlds

---
+++ paper-v1_19_R3/src/main/java/me/combimagnetron/lagoon/world/GameLevel.java
@@ -0,0 +1,179 @@
+package me.combimagnetron.lagoon.world;
+
+import com.mojang.serialization.Lifecycle;
+import me.combimagnetron.lagoon.data.Identifier;
+import net.minecraft.server.MinecraftServer;
+import net.minecraft.server.level.ServerLevel;
+import net.minecraft.server.level.progress.ChunkProgressListener;
+import net.minecraft.util.datafix.DataFixers;
+import net.minecraft.world.Difficulty;
+import net.minecraft.world.flag.FeatureFlagSet;
+import net.minecraft.world.level.*;
+import net.minecraft.world.level.chunk.ChunkStatus;
+import net.minecraft.world.level.dimension.DimensionType;
+import net.minecraft.world.level.dimension.LevelStem;
+import net.minecraft.world.level.levelgen.WorldOptions;
+import net.minecraft.world.level.storage.LevelStorageSource;
+import net.minecraft.world.level.storage.PrimaryLevelData;
+import org.apache.commons.io.FileUtils;
+import org.bukkit.Bukkit;
+import org.bukkit.World;
+import org.bukkit.block.Biome;
+import org.bukkit.block.data.BlockData;
+import org.bukkit.generator.BiomeProvider;
+import org.bukkit.generator.WorldInfo;
+import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.*;
+
+public class GameLevel {
+    private final ServerLevel serverLevel;
+    private static String name;
+
+    public GameLevel(Identifier identifier, MeridianWorld meridianWorld) {
+        this.serverLevel = new ServerLevel(
+                MinecraftServer.getServer(),
+                MinecraftServer.getServer().executor,
+                LevelAccess.levelStorageAccess(),
+                LevelData.primaryLevelData(),
+                Level.END,
+                LevelStem.END,
+                new ChunkProgressListenerDummy(),
+                false,
+                0L,
+                List.of(),
+                false,
+                World.Environment.THE_END,
+                ChunkGeneratorImpl.chunkGenerator(meridianWorld),
+                BiomeProviderImpl.biomeProvider()
+        );
+        name = "comet_" + identifier.string() + "_" + UUID.randomUUID().toString().substring(0, 6);
+    }
+
+    static final class LevelAccess {
+        public static LevelStorageSource.LevelStorageAccess levelStorageAccess() {
+            try {
+                final Path worldFolder;
+                worldFolder = Files.createTempDirectory(name).toAbsolutePath();
+                FileUtils.forceDeleteOnExit(worldFolder.toFile());
+                LevelStorageSource levelStorageSource = new LevelStorageSource(worldFolder, worldFolder, DataFixers.getDataFixer());
+                return levelStorageSource.new LevelStorageAccess(name, LevelStem.END);
+            } catch (IOException e) {
+                throw new RuntimeException(e);
+            }
+        }
+    }
+
+    static final class LevelData {
+
+        public static PrimaryLevelData primaryLevelData() {
+            return new PrimaryLevelData(
+                    new LevelSettings(name,
+                            GameType.SURVIVAL,
+                            false,
+                            Difficulty.NORMAL,
+                            true,
+                            new GameRules(),
+                            new WorldDataConfiguration(
+                                    new DataPackConfig(List.of(), List.of()),
+                                    FeatureFlagSet.of()
+                            )
+                    ),
+                    new WorldOptions(0L, false, false),
+                    PrimaryLevelData.SpecialWorldProperty.NONE,
+                    Lifecycle.stable()
+            );
+        }
+
+    }
+
+    static final class ChunkGeneratorImpl extends org.bukkit.generator.ChunkGenerator {
+        private final MeridianWorld meridianWorld;
+
+        private ChunkGeneratorImpl(MeridianWorld world) {
+            this.meridianWorld = world;
+        }
+
+        public static ChunkGeneratorImpl chunkGenerator(MeridianWorld meridianWorld) {
+            return new ChunkGeneratorImpl(meridianWorld);
+        }
+
+
+        @Override
+        public void generateNoise(WorldInfo worldInfo, Random random, int chunkX, int chunkZ, ChunkData chunkData) {
+            MeridianChunk meridianChunk = meridianWorld.chunkAt(chunkX, chunkZ);
+            Map<Integer, Set<BlockData>> blockData = new HashMap<>();
+            for (MeridianSection section : meridianChunk.sections()) {
+                int y = -64;
+                Set<BlockData> blockDataSet = new LinkedHashSet<>();
+                for (String string : section.blockPalette()) {
+                    blockDataSet.add(Bukkit.createBlockData(string));
+                }
+                blockData.put(y, blockDataSet);
+            }
+            for(int y = chunkData.getMinHeight(); y <= chunkData.getMaxHeight(); y++) {
+                Collection<BlockData> blockDataCollection = blockData.get(y);
+                for(int x = 0; x < 16; x++) {
+                    for(int z = 0; z < 16; z++) {
+                        chunkData.setBlock(x, y, z, blockDataCollection.iterator().next());
+                    }
+                }
+            }
+        }
+
+    }
+
+    static final class BiomeProviderImpl extends BiomeProvider {
+
+        private BiomeProviderImpl() {
+        }
+
+        public static BiomeProviderImpl biomeProvider() {
+            return new BiomeProviderImpl();
+        }
+
+        @Override
+        public @NotNull Biome getBiome(@NotNull WorldInfo worldInfo, int x, int y, int z) {
+            return Biome.END_BARRENS;
+        }
+
+        @Override
+        public @NotNull List<Biome> getBiomes(@NotNull WorldInfo worldInfo) {
+            return List.of(Biome.END_BARRENS);
+        }
+    }
+
+    static final class ChunkProgressListenerDummy implements ChunkProgressListener {
+
+        @Override
+        public void updateSpawnPos(ChunkPos spawnPos) {
+
+        }
+
+        @Override
+        public void onStatusChange(ChunkPos pos, @Nullable ChunkStatus status) {
+
+        }
+
+        @Override
+        public void start() {
+
+        }
+
+        @Override
+        public void stop() {
+
+        }
+
+        @Override
+        public void setChunkRadius(int radius) {
+
+        }
+    }
+
+
+}
